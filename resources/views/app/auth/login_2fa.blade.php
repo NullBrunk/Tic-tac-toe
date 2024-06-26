9d0085482c32a85c2dccc28aadebd8ca72abfc9a
NullBrunk <anasi10@proton.me>
2024-06-26 17:19:59 +0200

    [REFACTOR] Simplify UI of TOTP enter code page (add better support for backspace)

--- resources/views/app/auth/login_2fa.blade.php
+++ resources/views/app/auth/login_2fa.blade.php
@@ -48,59 +48,47 @@
     </section>
 
     <script>
-        // Ce bout de code permet d'automatiquement focus l'input suivant une fois qu'on en a rempli un
         let totp_inputs = [];
 
         for(let i = 1; i <= 6; i++) {
             // On met tous les inputs dans un tableau
             totp_inputs[i] = document.getElementById(`totp${i}`);
 
-            // On écoute l'événement input
+            /* ------- Focus l'input suivant quand on a mis un nombre dans l'input courant ------- */
             totp_inputs[i].addEventListener("input", (e) => {
-                // Si il est de type ajout de texte
-                if(e.inputType === "insertText") {
-                    // On récupère le next input
-                    let next_input = totp_inputs[i+1];
+                // Si il n'est pas de type ajout de texte, on quitte la fonction
+                if(e.inputType !== "insertText")
+                    return;
 
-                    // Si il existe
-                    if(next_input) {
-                        // On le focus
-                        next_input.focus();
-                    }
-                }
-                // else if(e.inputType === "deleteContentBackward") {
-                //     console.log(e.target);
-                //     let previous_input = totp_inputs[i-1];
-                //
-                //     if(previous_input) {
-                //         previous_input.focus();
-                //     }
-                //
-                // }
+                // Si il est de type ajout de texte, on ne prevent pas le comportement par défaut (ajouter du texte dans l'input)
+
+                // On récupère le next input
+                let next_input = totp_inputs[i+1];
+
+
+                // Si il existe on le focus
+                next_input && next_input.focus();
             });
 
+            /* ------- Focus l'input précédent quand on efface un nombre dans un input vide ------- */
             totp_inputs[i].addEventListener("keydown", (e) => {
-                // Si l'utilisateur presse backspace
+
+                // Si l'utilisateur presse la touche effacter
                 if(e.key === "Backspace") {
 
-                    // On va vérifier si l'input sur lequel il est contient quelque chose ou pas
-                    // si il contient un nombre -> ne rien faire (laisser le comportement par défaut)
-                    // Sinon, retourner un input derriere
+                    // Si l'input sur lequel l'utilisateur est est vide
                     if(e.target.value === "") {
                         let new_input = totp_inputs[i-1];
 
+                        // On focus le précédent si il existe
                         if(new_input) {
                             new_input.focus();
+                            // et on efface son contenu
                             new_input.value = '';
-
                         }
                     }
                 }
             });
-
-            totp_inputs[i].addEventListener("focus", (e) => {
-                this.selectionStart = this.selectionEnd = 10000;
-            })
         }
     </script>
 @endsection
